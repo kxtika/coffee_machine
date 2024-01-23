060f90dfb4e3165d8380ef47a732820ac0b2a625
kt201 <katiaminiaylo@gmail.com>
2024-01-23 20:10:55 +0200

    Added input sanitizing and manager's functions

--- main.py
+++ main.py
@@ -23,25 +23,34 @@ COFFEE_DRINKS = [
      "cost": 1.5},
 ]
 
+machine_off = False
+
 # TODO: 1. Set measures of liquids for different coffee drinks
 current_water = 300
 current_coffee = 100
 current_milk = 200
 
+revenue = 0
 
-def get_data(drink_number, lst):
 
+def sanitize_input(number):
     # Input sanitizing
-    if not drink_number.isnumeric():
+    if not number.isnumeric():
         print(config.DRINK_ERROR_MESSAGE)
         sys.exit(0)
     else:
-        drink_number = int(user_drink)
-        if drink_number > 3 or drink_number <= 0:
+        number = int(number)
+        if number > 3 or number <= 0:
             print(config.DRINK_ERROR_MESSAGE)
             sys.exit(0)
 
-    drink_dict = lst[drink_number-1]
+    return number
+
+
+def get_data(drink_number, lst):
+    drink_number = sanitize_input(drink_number)
+
+    drink_dict = lst[drink_number - 1]
     water_amount = drink_dict["water"]
     coffee_amount = drink_dict["coffee"]
     milk_amount = drink_dict["milk"]
@@ -83,22 +92,37 @@ def subtract_money(tpl, money):
 
 
 # TODO: . Check if the transaction was successful
-def is_transaction_successful():
-    pass
+def is_transaction_successful(change):
+    return change >= 0
 
 
-# TODO: . Ask the customer to choose a drink
-user_drink = input(config.ASK_FOR_COFFEE)
+while not machine_off:
+    # TODO: . Ask a customer to choose a drink
+    user_drink = input(config.ASK_FOR_COFFEE)
 
-# TODO:
-drink_data = get_data(user_drink, COFFEE_DRINKS)
-coffee = subtract_measures(drink_data)
+    # TODO:
+    drink_data = get_data(user_drink, COFFEE_DRINKS)
+    current_water, current_coffee, current_milk = subtract_measures(drink_data)
+
+    given_money = count_money()
+    print(given_money)
+    user_change = subtract_money(drink_data, given_money)
+    print(user_change)
+
+    # TODO: . Check if the transaction was successful
+    if is_transaction_successful(user_change):
+        revenue += drink_data[3]  # Add the cost of the drink to revenue
 
 
-given_money = count_money()
-print(given_money)
-user_change = subtract_money(drink_data, given_money)
-print(user_change)
+    manager_functions = input("If you want to see the report, press '1'. To see revenue, press '2'. "
+                              "To turn off the machine press '3'.")
 
-# TODO: 2. Report about the amount of milk, water, and coffee
-print(f"There are {current_water}ml of water, {current_coffee}g of coffee, and {current_milk}ml of milk left.")
+    sanitize_input(manager_functions)
+
+    if manager_functions == 1:
+        # TODO: 2. Report about the amount of milk, water, and coffee
+        print(f"There are {current_water}ml of water, {current_coffee}g of coffee, and {current_milk}ml of milk left.")
+    elif manager_functions == 2:
+        print(f"The revenue is ${revenue}.")
+    elif manager_functions == 3:
+        machine_off = True
